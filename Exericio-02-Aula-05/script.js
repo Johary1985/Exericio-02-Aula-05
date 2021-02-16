1f48ea0a00924908795783bd661fec76c8abc08a
Johary1985 <77841530+Johary1985@users.noreply.github.com>
2021-02-16 14:37:33 -0300

    Add files via upload

---
+++ Exericio-02-Aula-05/script.js
@@ -0,0 +1,18 @@
+let PrimerUsuario = prompt ("PrimerUsuario")
+let SegundoUsuario = prompt ("SegundoUsuario")
+let Turma= prompt ("Turma")
+let Turma2 = prompt ("Turma2")
+let Modulo = prompt ("Modulo")
+let Modulo2 = prompt ("Modulo2")
+
+if (PrimerUsuario == SegundoUsuario) {
+    console.log ("Usuario ja Cadastrado")
+} 
+
+if (Turma == Turma2) {
+    console.log ("Sao da Mesma Turma")
+} 
+
+if (Modulo == Modulo2) {
+    console.log ("Sao do mesmo Modulo")
+} 
